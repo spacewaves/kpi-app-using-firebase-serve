bde248354b1e72599c629f721f10a7e6bb85dfe4
Alexandre Shafii <alexandre.shafii@gmail.com>
2020-10-30 15:12:58 +0100

    feat: tasks can be added to firestore

--- public/src/index.js
+++ public/src/index.js
@@ -55,6 +55,23 @@ const handleClick = () => {
   let lastTaskAdded = addedTasks[addedTasks.length - 1];
   console.log(firebase);
   updateUI(lastTaskAdded);
+
+  const db = firebase.firestore();
+
+  const createThing = document.createElement("createThing");
+
+  const thingsList = document.getElementById("thingsList");
+
+  let thingsRef;
+
+  thingsRef = db.collection("things");
+  console.log("yo");
+
+  thingsRef.add({
+    taskName: taskName,
+    quantity: quantity,
+    date: date,
+  });
 };
 
 const updateUI = (item) => {
@@ -110,3 +127,18 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+  } else {
+    // not signed in
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML = "";
+  }
+});
+
+///// Firestore /////
